8c0bc640505eb5f8254487554f4f30263f54b470
Dhesheswar K <115608879+Dhesheswar@users.noreply.github.com>
2026-09-25 14:41:43 +0000

    changed rwuest param

--- simpleWebApp/src/main/java/com/spring/simpleWebApp/repository/ProductRepo.java
+++ simpleWebApp/src/main/java/com/spring/simpleWebApp/repository/ProductRepo.java
@@ -4,6 +4,7 @@ import com.spring.simpleWebApp.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
